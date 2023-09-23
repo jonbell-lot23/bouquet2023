7516dad90ff4c965af1b4a6130ac36f685188067
Jon Bell <github@lot23.com>
2023-09-23 22:43:56 +1200

    New test difference

--- cypress/e2e/1-basic/cookies.cy.js
+++ cypress/e2e/1-basic/cookies.cy.js
@@ -1,4 +1,5 @@
 it("cy.setCookie() - set a browser cookie", () => {
   cy.getCookies().should("be.empty");
-  cy.setCookie("jon").should("have.property", "value", "is cool");
+  cy.setCookie("jon", "is cool");
+  cy.getCookie("jon").should("have.property", "value", "is cool");
 });
